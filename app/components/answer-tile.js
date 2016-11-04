6bc8332ec8c31407f40470d513c88a6373b7bb5f
Ethan Law <ejlaw01@gmail.com>
2016-11-04 13:15:12 -0700

    sort answer by number of upvotes with computed property

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // upvotes: Ember.computed('answer.upvotes', function() {
-  //   return this.set('answer.upvotes', (this.get('answer.upvotes') + 1));
-  // }),
+
+  sortBy: ['upvotes:desc'],
+  sortedAnswers: Ember.computed.sort('question.answers', 'sortBy'),
+
+  actions: {
+    upvote(answer) {
+      this.sendAction('upvote', answer);
+    }
+  }
 });
