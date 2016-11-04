f75e13dec8d2f880f2503126d9cc9d8b293bebaf
Ethan Law <ejlaw01@gmail.com>
2016-11-04 14:37:24 -0700

    add helper to display a fire glyphicon next to questions in the index with more than 3 answers

---
+++ app/helpers/best-answer.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export function bestAnswer(params) {
+  var question = params[0];
+  // var answers = question.get('answers');
+
+  if(question.get('answers').get('length') >= 3) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+    }
+  }
+
+  // for (var i = 0; i < question.get('answers').get('length'); i++) {
+  //   if (answers[i].upvotes > 5) {
+  //     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+  //   }
+  // }
+
+export default Ember.Helper.helper(bestAnswer);
