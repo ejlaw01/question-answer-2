62e5c8d0203d88b124e689b2ce038f658f89cb58
Ethan Law <ejlaw01@gmail.com>
2016-11-04 10:21:11 -0700

    put answer tile into its own component and add nonfunctional button for upvotes

--- app/components/answer-question.js
+++ app/components/answer-question.js
@@ -10,6 +10,7 @@ export default Ember.Component.extend({
       var  params = {
         author: this.get('author'),
         response: this.get('response'),
+        upvotes: 0,
         question: this.get('question')
       };
       this.set('addNewAnswer', false);
